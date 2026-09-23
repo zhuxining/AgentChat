6990f7c614dbf8fe5b8de6a72f6ce2ab7caf10af
syneju <syneju@gmial.com>
2026-09-23 14:56:24 +0800

    chore(tauri): restore shell, SQL, and WebSocket plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,9 @@ mod commands;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_websocket::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(
             tauri_plugin_stronghold::Builder::new(|pass| {
